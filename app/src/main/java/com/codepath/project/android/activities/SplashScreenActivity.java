51922af538dd8926a8cf5a6fe40799c0fa7e211c
skasabar <skasabar@cisco.com>
2016-11-10 15:28:10 -0800

    // placeholder splash screen completed

---
+++ app/src/main/java/com/codepath/project/android/activities/SplashScreenActivity.java
@@ -0,0 +1,38 @@
+package com.codepath.project.android.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.codepath.project.android.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    int SPLASH_TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(SPLASH_TIME);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}

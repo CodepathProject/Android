f0252f8d83e1294aa0e27d4aa2dc823d665d6f20
sraveesh <sraveesh@ebay.com>
2016-11-12 14:50:11 -0800

    add review to products

--- app/src/main/java/com/codepath/project/android/activities/ProductViewActivity.java
+++ app/src/main/java/com/codepath/project/android/activities/ProductViewActivity.java
@@ -1,12 +1,17 @@
 package com.codepath.project.android.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.codepath.project.android.R;
+import com.codepath.project.android.fragments.ComposeFragment;
 import com.codepath.project.android.model.Product;
 import com.parse.ParseQuery;
 import com.squareup.picasso.Picasso;
@@ -20,7 +25,11 @@ public class ProductViewActivity extends AppCompatActivity {
     ImageView ivProductImage;
     @BindView(R.id.tvProductName)
     TextView tvProductName;
-    @BindView(R.id.tvBrandName) TextView tvBrandName;
+    @BindView(R.id.tvBrandName)
+    TextView tvBrandName;
+
+    Product product;
+    String productId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +37,38 @@ public class ProductViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_product_view);
         ButterKnife.bind(this);
 
-        String productId = getIntent().getStringExtra("productId");
+        if(savedInstanceState == null) {
+            productId = getIntent().getStringExtra("productId");
 
-        ParseQuery<Product> query = ParseQuery.getQuery("Product");
-        query.getInBackground(productId, (product, e) -> {
-            if (product == null) {
-                Log.d("score", "The getFirst request failed.");
-            } else {
-                Picasso.with(this).load(product.getImageUrl()).into(ivProductImage);
-                tvProductName.setText(product.getName());
-                tvBrandName.setText(product.getBrand());
-            }
-        });
+            ParseQuery<Product> query = ParseQuery.getQuery(Product.class);
+            query.setCachePolicy(ParseQuery.CachePolicy.CACHE_ELSE_NETWORK);
+            query.getInBackground(productId, (p, e) -> {
+                if (e == null) {
+                    product = p;
+                    Picasso.with(this).load(product.getImageUrl()).into(ivProductImage);
+                    tvProductName.setText(product.getName());
+                    tvBrandName.setText(product.getBrand());
+                } else {
+                    Toast.makeText(ProductViewActivity.this, "parse error", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    public void onAddReview(View view) {
+        Bundle bundle = new Bundle();
+        bundle.putString("productId", product.getObjectId());
+
+        FragmentTransaction fm = getSupportFragmentManager().beginTransaction();
+        ComposeFragment composeFragment = ComposeFragment.newInstance("Add review");
+        composeFragment.setArguments(bundle);
+        composeFragment.setStyle(DialogFragment.STYLE_NORMAL, R.style.Dialog_FullScreen);
+        composeFragment.show(fm, "fragment_compose");
     }
 }
---
+++ app/src/main/java/com/codepath/project/android/fragments/ComposeFragment.java
@@ -0,0 +1,104 @@
+package com.codepath.project.android.fragments;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.codepath.project.android.R;
+import com.codepath.project.android.activities.ProductViewActivity;
+import com.codepath.project.android.model.Product;
+import com.codepath.project.android.model.Review;
+import com.parse.ParseQuery;
+
+public class ComposeFragment extends DialogFragment {
+
+    private EditText etReviewText;
+    private Button btnPost;
+    Product product;
+
+    public ComposeFragment() {}
+
+    public static ComposeFragment newInstance(String title) {
+        ComposeFragment frag = new ComposeFragment();
+        Bundle args = new Bundle();
+        args.putString("Compose review", title);
+        frag.setArguments(args);
+        return frag;
+    }
+
+    @Override
+    public void onResume() {
+        ViewGroup.LayoutParams params = getDialog().getWindow().getAttributes();
+        params.width = WindowManager.LayoutParams.MATCH_PARENT;
+        params.height = WindowManager.LayoutParams.MATCH_PARENT;
+        getDialog().getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+        super.onResume();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_compose, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Get field from view
+        etReviewText = (EditText) view.findViewById(R.id.etReviewText);
+        btnPost = (Button) view.findViewById(R.id.btnPost);
+        ImageView ivComposeCancel = (ImageView) view.findViewById(R.id.ivComposeCancel);
+
+        ivComposeCancel.setOnClickListener(v -> {
+            InputMethodManager imm =(InputMethodManager)getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            Intent intent = new Intent(getActivity(), ProductViewActivity.class);
+            startActivity(intent);
+        });
+
+        String title = getArguments().getString("title", "Compose review");
+        getDialog().setTitle(title);
+        etReviewText.requestFocus();
+        getDialog().getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE | WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        InputMethodManager imgr = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imgr.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+
+        Bundle bundle = this.getArguments();
+        String productId = bundle.getString("productId");
+        ParseQuery<Product> query = ParseQuery.getQuery(Product.class);
+        query.setCachePolicy(ParseQuery.CachePolicy.CACHE_ELSE_NETWORK);
+        query.getInBackground(productId.trim(), (p, e) -> {
+            if(e == null) {
+                product = p;
+            } else {
+                Toast.makeText(getActivity(), "parse error", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnPost.setOnClickListener(arg0 -> {
+            Review review = new Review();
+            review.setText(etReviewText.getText().toString());
+            product.incrementReviewCount();
+            review.setProduct(product);
+            review.saveInBackground();
+            InputMethodManager imm =(InputMethodManager)getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            Intent intent = new Intent(getActivity(), ProductViewActivity.class);
+            startActivity(intent);
+        });
+    }
+}

2778d34baab8adf765f8b79599953878bf0f3cd8
amallya18 <aniruddha.mallya@oracle.com>
2016-11-11 17:20:29 -0800

    Add test data for scroll view

---
+++ app/src/main/java/com/codepath/project/android/data/TestData.java
@@ -0,0 +1,20 @@
+package com.codepath.project.android.data;
+
+/**
+ * Created by anmallya on 11/11/2016.
+ */
+
+public class TestData {
+
+    public static final String [] itemName = {"HP Notebook 14-ax010nr", "32GB USB Flash Drive", "32GB USB Flash Drive"};
+
+    public static final String [] itemUrl = {"http://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05218075.png",
+            "http://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c04381013.png",
+            "http://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c04478617.png"};
+
+    public static final String [] reviewItemName = {"Original Ink Cartridges", "HP Premium Plus Glossy Photo Paper", "HP Sprocket Photo Printer"};
+
+    public static final String [] reviewItemUrl = {"http://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c02436844.png",
+            "http://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c02702779.png",
+            "http://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05266447.png"};
+}

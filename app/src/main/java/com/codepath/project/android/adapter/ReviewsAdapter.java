04fff0808afb6e04e682e18e727723f7b7abb634
Sandeep Raveesh <crsandy@gmail.com>
2016-11-15 21:10:32 -0800

    added gravatar

--- app/src/main/java/com/codepath/project/android/adapter/ReviewsAdapter.java
+++ app/src/main/java/com/codepath/project/android/adapter/ReviewsAdapter.java
@@ -7,13 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.codepath.project.android.R;
 import com.codepath.project.android.model.Review;
+import com.codepath.project.android.utils.GeneralUtils;
 import com.parse.ParseException;
-import com.parse.ParseFile;
 import com.parse.ParseUser;
 import com.squareup.picasso.Picasso;
 
@@ -38,8 +37,8 @@ public class ReviewsAdapter extends
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.ivProfile) ImageView ivProfile;
         @BindView(R.id.tvReview) TextView tvReview;
-        @BindView(R.id.llImages) LinearLayout llImages;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -52,8 +51,7 @@ public class ReviewsAdapter extends
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
         View contactView = inflater.inflate(R.layout.item_review, parent, false);
-        ViewHolder viewHolder = new ViewHolder(contactView);
-        return viewHolder;
+        return new ViewHolder(contactView);
     }
 
     @Override
@@ -61,39 +59,46 @@ public class ReviewsAdapter extends
         Review review = mReviews.get(position);
         TextView tvReview = viewHolder.tvReview;
         try {
-            ParseUser user = review.getUser().fetchIfNeeded();
-            String formattedText = "<b>" + user.getString("firstName") + ": </b> " + review.getText();
+            ParseUser user = review.getUser();
+            String formattedText = "";
+            if(user != null) {
+                user = user.fetchIfNeeded();
+                formattedText += "<b>" + user.getString("firstName") + ": </b> ";
+                Picasso.with(getContext()).load(GeneralUtils.getProfileUrl(user.getObjectId())).into(viewHolder.ivProfile);
+            }
+            formattedText+= review.getText();
             tvReview.setText(Html.fromHtml(formattedText));
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
-        if(review.getImages() != null && review.getImages().size() > 0) {
-            viewHolder.llImages.setVisibility(View.VISIBLE);
-            viewHolder.llImages.removeAllViews();
-            for(ParseFile image: review.getImages()) {
-//                ParseImageView ivImage = new ParseImageView(getContext());
+//        if(review.getImages() != null && review.getImages().size() > 0) {
+//            viewHolder.llImages.setVisibility(View.VISIBLE);
+//            viewHolder.llImages.removeAllViews();
+//            for(ParseFile image: review.getImages()) {
+////                ParseImageView ivImage = new ParseImageView(getContext());
+////                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(500, 500);
+////                layoutParams.setMarginEnd(30);
+////                ivImage.setLayoutParams(layoutParams);
+////                ivImage.setParseFile(image);
+////                viewHolder.llImages.addView(ivImage);
+//
+//
+//                ImageView ivImage = new ImageView(getContext());
 //                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(500, 500);
 //                layoutParams.setMarginEnd(30);
 //                ivImage.setLayoutParams(layoutParams);
-//                ivImage.setParseFile(image);
+//                Picasso.with(getContext()).load(image.getUrl()).into(ivImage);
 //                viewHolder.llImages.addView(ivImage);
-
-
-                ImageView ivImage = new ImageView(getContext());
-                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(500, 500);
-                layoutParams.setMarginEnd(30);
-                ivImage.setLayoutParams(layoutParams);
-                Picasso.with(getContext()).load(image.getUrl()).into(ivImage);
-                viewHolder.llImages.addView(ivImage);
-            }
-        } else {
-            viewHolder.llImages.setVisibility(View.GONE);
-        }
+//            }
+//        } else {
+//            viewHolder.llImages.setVisibility(View.GONE);
+//        }
     }
 
     @Override
     public int getItemCount() {
         return mReviews.size();
     }
+
 }
--- app/src/main/java/com/codepath/project/android/utils/GeneralUtils.java
+++ app/src/main/java/com/codepath/project/android/utils/GeneralUtils.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
 
 public class GeneralUtils {
 
@@ -35,5 +37,18 @@ public class GeneralUtils {
         } catch (IOException e)          { e.printStackTrace(); }
         catch (InterruptedException e) { e.printStackTrace(); }
         return false;
+    }
+
+    public static String getProfileUrl(final String userId) {
+        String hex = "";
+        try {
+            final MessageDigest digest = MessageDigest.getInstance("MD5");
+            final byte[] hash = digest.digest(userId.getBytes());
+            final BigInteger bigInt = new BigInteger(hash);
+            hex = bigInt.abs().toString(16);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "http://www.gravatar.com/avatar/" + hex + "?d=identicon";
     }
 }

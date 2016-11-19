bddab6fdf19a8a04f3d469f9127667d01c7e06cb
amallya13 <aniruddha.mallya@oracle.com>
2016-11-18 20:08:26 -0800

    Edit Manifest file; Add text data;

--- app/src/main/java/com/codepath/project/android/activities/CategoryDetailActivity.java
+++ app/src/main/java/com/codepath/project/android/activities/CategoryDetailActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import com.codepath.project.android.R;
 import com.codepath.project.android.adapter.CategoryDetailFragmentAdapter;
 
+import static com.codepath.project.android.data.TestData.tabNameList;
+
 public class CategoryDetailActivity extends AppCompatActivity {
 
     ViewPager viewPager;
@@ -19,18 +21,14 @@ public class CategoryDetailActivity extends AppCompatActivity {
         setupTabs();
     }
 
-    String[] list = {"Samsung","Apple","Sony","LG"};
-
     private void setupTabs(){
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewPager.setAdapter(new CategoryDetailFragmentAdapter(getSupportFragmentManager(),
                 this));
         TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager);
-        System.out.println("############## list "+tabLayout.getTabCount());
         for (int i = 0; i < tabLayout.getTabCount(); i++) {
-            System.out.println("############## list "+list[i]);
-            tabLayout.getTabAt(i).setText(list[i]);
+            tabLayout.getTabAt(i).setText(tabNameList[i]);
         }
     }
 }

2b5018e0d713ae40c2e4e3c6ad15e038dd44bce4
Sandeep Raveesh <crsandy@gmail.com>
2016-11-19 21:50:17 -0800

    search feature with autocomplete

--- app/src/main/java/com/codepath/project/android/activities/HomeActivity.java
+++ app/src/main/java/com/codepath/project/android/activities/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.codepath.project.android.activities;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -18,15 +20,20 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.codepath.project.android.R;
+import com.codepath.project.android.adapter.SearchResultsAdapter;
 import com.codepath.project.android.fragments.HomeFragment;
 import com.codepath.project.android.fragments.MyProductsFragment;
+import com.codepath.project.android.model.Product;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class HomeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, SearchView.OnSuggestionListener {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
@@ -34,6 +41,12 @@ public class HomeActivity extends AppCompatActivity
     DrawerLayout drawer;
     @BindView(R.id.nav_view)
     NavigationView navigationView;
+
+    public static String[] columns = new String[]{"_id", "productId", "name", "image"};
+
+    SearchView searchView;
+
+    SearchResultsAdapter mSearchViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,8 +88,11 @@ public class HomeActivity extends AppCompatActivity
     }
 
     private void setSearchView(MenuItem searchItem) {
-        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
         searchView.requestFocus();
+        searchView.setOnSuggestionListener(this);
+        mSearchViewAdapter = new SearchResultsAdapter(this, R.layout.search_result_list_item, null, columns, null, -1000);
+        searchView.setSuggestionsAdapter(mSearchViewAdapter);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -86,9 +102,39 @@ public class HomeActivity extends AppCompatActivity
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                return false;
+                if (newText.length() > 1) {
+                    loadSearchData(newText);
+                }
+                return true;
             }
         });
+    }
+
+    private void loadSearchData(String newText) {
+        ParseQuery<Product> query = ParseQuery.getQuery(Product.class);
+        query.whereMatches("name", newText, "i");
+        query.setLimit(5);
+        query.findInBackground((objects, e) -> {
+            if (e == null) {
+                MatrixCursor matrixCursor = convertToCursor(objects);
+                mSearchViewAdapter.changeCursor(matrixCursor);
+            }
+        });
+    }
+
+    private MatrixCursor convertToCursor(List<Product> products) {
+        MatrixCursor cursor = new MatrixCursor(columns);
+        int i = 0;
+        for (Product product : products) {
+            String[] temp = new String[4];
+            i = i + 1;
+            temp[0] = Integer.toString(i);
+            temp[1] = product.getObjectId();
+            temp[2] = product.getName();
+            temp[3] = product.getImageUrl();
+            cursor.addRow(temp);
+        }
+        return cursor;
     }
 
     private void setNavigationDrawer() {
@@ -139,6 +185,26 @@ public class HomeActivity extends AppCompatActivity
         item.setChecked(true);
         setTitle(item.getTitle());
 
+        return true;
+    }
+
+    @Override
+    public boolean onSuggestionSelect(int position) {
+        return goToProductDetailView(position);
+    }
+
+    @Override
+    public boolean onSuggestionClick(int position) {
+        return goToProductDetailView(position);
+    }
+
+    private boolean goToProductDetailView(int position) {
+        Cursor cursor = (Cursor) searchView.getSuggestionsAdapter().getItem(position);
+        String productId = cursor.getString(1);
+        searchView.clearFocus();
+        Intent intent = new Intent(this, ProductViewActivity.class);
+        intent.putExtra("productId", productId);
+        startActivity(intent);
         return true;
     }
 }

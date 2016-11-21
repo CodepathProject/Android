0c813979ac75c56e5ffcfcbe5537d45bba44c8c5
skasabar <skasabar@cisco.com>
2016-11-20 21:39:00 -0800

    // user detail setup

---
+++ app/src/main/java/com/codepath/project/android/activities/UserDetailActivity.java
@@ -0,0 +1,16 @@
+package com.codepath.project.android.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.codepath.project.android.R;
+
+public class UserDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_detail);
+    }
+}
+
